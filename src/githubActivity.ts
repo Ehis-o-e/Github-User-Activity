6d4cb434c25c6a73fbb778d4dbc98146acfa7d18
Ehis-o-e <his.ehinomhen@gmail.com>
2025-08-10 01:34:15 +0100

    API COMMUNICATION

--- src/githubActivity.ts
+++ src/githubActivity.ts
@@ -1,4 +1,5 @@
 import { argv } from "process";
+import https from 'https';
 
 function main(){
     const userName = argv[2];
@@ -13,6 +14,37 @@ function main(){
         console.log('Error: Username cannot be empty')
         process.exit(1);
     }
-    console.log(`Welcome ${trim}`);
+
+    const options = {
+        headers: {
+            'User-Agent': 'GitHub-User-Activity-Fetcher()' 
+        }
+    };
+
+    const url = `https://api.github.com/users/${trim}/events`;
+    console.log(url)
+
+    https.get(url, options, (res)=>{
+
+        let rawData = "";
+        res.on('data', (chunk)=> {
+            rawData += chunk
+        });
+
+        res.on('end', ()=>{
+            try {
+                const parseData = JSON.parse(rawData);
+                
+                console.log(parseData);
+            }catch(err){
+                console.log("Failed to parse JSON", err)
+            }
+        });
+
+        res.on("error", (err)=>{
+            console.error("Failed to load", err)
+        })
+    })
+    
 }
 main();
